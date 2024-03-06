dd4ec53a29b6b656beca79846d9091e7a2746a46
Darren Draper <darren.j.draper@gmail.com>
2024-03-06 21:28:16 +1000

    Code-play 4: Rendering a list of objects

--- Module5/my-app/src/App.js
+++ Module5/my-app/src/App.js
@@ -1,12 +1,56 @@
+const animals = [
+  {
+    name: "Lion",
+    number: 3,
+    eats: ["zerba", "antelope", "buffalo", "hippopotamus"],
+  },
+  {
+    name: "Tiger",
+    number: 5,
+    eats: ["moose", "deer", "buffalo"],
+  },
+  {
+    name: "Giraffe",
+    number: 6,
+    eats: ["leaves", "twigs"],
+  },
+  {
+    name: "Elephant",
+    number: 4,
+    eats: ["grass", "leaves", "flowers", "fruit"],
+  },
+  {
+    name: "Monkey",
+    number: 10,
+    eats: ["fruit", "leaves", "vegetables", "insects"],
+  },
+  {
+    name: "Lemur",
+    number: 15,
+    eats: ["fruit", "leaves", "insects"],
+  },
+  {
+    name: "Rhinoceros",
+    number: 2,
+    eats: ["grass", "shoots", "leaves", "berries"],
+  },
+];
+
+
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
       <h2>Start editing to see some magic happen!</h2>
+      {animals.map((animal) => (
+        <AnimalComponent name={animal.name} number={animal.number} />
+      ))}
       <AnimalComponent name="Lion" number={35} />
     </div>
   );
 }
+
 function AnimalComponent(props) {
   return (
     <div>
