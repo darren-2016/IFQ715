d4097db7035cea1bbc3f1488977f3e6bbebb1d43
Darren Draper <darren.j.draper@gmail.com>
2024-03-07 15:35:51 +1000

    Activity 5.8: Create a book-rating app

--- Module5/book-rating-app/src/App.js
+++ Module5/book-rating-app/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from "react";
+
+const books = [
+  {
+    title: "1984",
+    author: "George Orwell",
+  },
+  {
+    title: "The Magician's Nephew",
+    author: "C. S. Lewis",
+  },
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+  },
+  {
+    title: "The Book Thief",
+    author: "Markus Zusak",
+  },
+ ];
+
+ function Book(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.author}</p>
+      <LikeCounter/>
     </div>
   );
 }
 
-export default App;
+// function Book({ title, author }) {
+//   return (
+//     <div>
+//       <h1>{title}</h1>
+//       <p>{author}</p>
+//     </div>
+//   );
+// }
+
+export default function App() {
+  return (
+    <div className="App">
+      
+      {/* {books.map((book) => (
+      <Book title={book.title} author={book.author} />
+        ))} */}
+
+      {books.map((book) => (
+      <Book {...book} />
+        ))}
+    </div>
+  );
+}
+
+function LikeCounter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((oldCount) => oldCount + 1);
+  };
+  const decrement = () => {
+    setCount((oldCount) => oldCount - 1);
+  };
+  return (
+    <div>
+      <p>Like Count: {count}</p>
+      <button onClick={increment}>Like</button>
+      <button onClick={decrement}>Dislike</button>
+    </div>
+  );
+}
