e2a64d3e22db97fc4b3125b19120538c4665f730
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 15:56:17 +1000

    Formatting of footer

--- Module6/my-resume/src/Components/Footer.js
+++ Module6/my-resume/src/Components/Footer.js
@@ -1,3 +1,16 @@
+import { Row, Col } from "react-bootstrap";
+
 export default function Footer() {
-    return <p>Footer</p>;
+    return (
+        <footer className="text-bg-light mt-auto p-5">
+            <Row>
+                <Col md={6}>
+                    <p>&copy; 2024, Darren J. Draper</p>
+                </Col>
+                <Col md={6}>
+                    <p className="fw-light">More Footer text...</p>
+                </Col>
+            </Row>
+        </footer>
+    );
 }
