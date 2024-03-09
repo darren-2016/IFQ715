c90ced3acc2606ad7e3109e8819c07d7be0e7287
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 20:38:28 +1000

    Code-play 1: Consuming promises by sending a request with Fetch

--- Module7/my-app2/src/App.js
+++ Module7/my-app2/src/App.js
@@ -3,21 +3,22 @@ import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+function fetchUser() {
+  const url = `https://reqres.in/api/users/3`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
 
 function App() {
-  const janet = {
-    id: 2,
-    email: "janet.weaver@reqres.in",
-    first_name: "Janet",
-    last_name: "Weaver",
-    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg",
-  };
   const [user, setUser] = useState({});
 
   return (
     <div className="App">
       <h1>User Details</h1>
-      <button onClick={() => setUser(janet)}>Get User</button>
+      <button onClick={() => fetchUser().then((user) => setUser(user))}>
+        Get User
+      </button>
       <ul>
         <li>First Name: {user.first_name}</li>
         <li>Last Name: {user.last_name}</li>
