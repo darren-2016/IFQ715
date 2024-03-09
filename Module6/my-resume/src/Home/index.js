750580d1591408a3605677a68af05718cb7e4ff3
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 17:36:42 +1000

    More updates to portfolio

--- Module6/my-resume/src/Home/index.js
+++ Module6/my-resume/src/Home/index.js
@@ -1,3 +1,17 @@
 export default  function Home() {
-    return <h1>Home</h1>;
+    return (
+        <div>
+            <h1>Home</h1>
+            <h2>Programming Languages</h2>
+            <ul>
+                <li>
+                    C
+                </li>
+                <li>
+                    C++
+                </li>
+
+            </ul>
+        </div>
+    );
 }
