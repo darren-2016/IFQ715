c0aa724e0974f7ec0eaefba63d9e18cb909afef6
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 22:35:52 +1000

    Code-play 3: Uncontrolled and controlled components

--- Module7/my-app3/src/App.js
+++ Module7/my-app3/src/App.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <label htmlFor="name">Your Name:</label>
+
+//       <form onSubmit={(event) => {
+//         event.preventDefault();
+
+//         console.log(event.target.elements.name.value);
+//       }}>
+//         <label htmlFor="name">Your Name:</label>
+//         <input id="name" name="name" type="text" />
+//       </form>
+//     </div>
+//   );
+// }
+
 function App() {
+  const [name, setName] = useState("");
+
+
   return (
     <div className="App">
-      <label htmlFor="name">Your Name:</label>
-      <form onSubmit={(event) => {
-        event.preventDefault();
 
-        console.log(event.target.elements.name.value);
-      }}>
-        <label htmlFor="name">Your Name:</label>
-        <input id="name" name="name" type="text" />
-      </form>
+      <h1>Hello, {name}!</h1>
+
+      <label htmlFor="name">Your Name:</label>
+      <input type="text" name="name" id="name" value={name} onChange={(event) => { setName(event.target.value)}} />
+
     </div>
   );
 }
