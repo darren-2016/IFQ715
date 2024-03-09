5d3c8d326b6a62717173a5b57bdbe36acef7993c
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 16:24:44 +1000

    Nav bar added

--- Module6/my-resume/src/Components/Header.js
+++ Module6/my-resume/src/Components/Header.js
@@ -1,11 +1,35 @@
-import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, useResolvedPath, useMatch } from "react-router-dom";
+
+function HighlightLink(props) {
+    let resolved = useResolvedPath(props.to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+    return <Nav.Link {...props} active={match} />;
+}
 
 export default function Header() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
+        <Navbar bg="primary" expand="md" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="#">My Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarSupportedContent" />
+                <Navbar.Collapse id="navbarSupportedContent">
+                    <Nav className="me-auto">
+                        <HighlightLink to="/" as={Link}>
+                            Home
+                        </HighlightLink>
+                        <HighlightLink to="/contact" as={Link}>
+                            Contact
+                        </HighlightLink>
+                        {/* <Nav.Link href="#"to="/">Home</Nav.Link>
+                        <Nav.Link to="/contact">Contact</Nav.Link> */}
+                    </Nav>
+                </Navbar.Collapse>
+                
+            </Container>
+        </Navbar>
     );
 }
 
