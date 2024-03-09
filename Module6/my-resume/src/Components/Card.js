91363d593db0d53b578a23160155a250ce14e792
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 14:19:37 +1000

    Modifiying format of card, adding header

--- Module6/my-resume/src/Components/Card.js
+++ Module6/my-resume/src/Components/Card.js
@@ -3,17 +3,15 @@ import { Container, Button, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
-export default function BusinessCard () {
+export default function BusinessCard ( {name, title, description} ) {
     return (
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+          <Card.Header>{name}</Card.Header>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Software Engineer</Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            {/* <Card.Title>{ name }</Card.Title> */}
+            <Card.Subtitle className="mb-2 text-muted">{ title }</Card.Subtitle>
+            <Card.Text>{ description }</Card.Text>
             <Card.Link href="#">Email</Card.Link>
             <Card.Link href="#">LinkedIn</Card.Link>
 
--- Module6/my-resume/src/Contact/index.js
+++ Module6/my-resume/src/Contact/index.js
@@ -3,6 +3,7 @@ import BusinessCard from "../Components/Card";
 export default function Contact() {
     // return <h1>Contact</h1>;
     return (
-        <BusinessCard />
+        <BusinessCard name="Darren Draper" title="Software Engineer" 
+        description="I am a software engineer with 30 years of software development experience" />
     )
 }
