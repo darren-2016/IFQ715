282e6867f9da562c0ac83ce0956678b1aaa6790d
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 21:13:36 +1000

    Code-play 2: Introducing side effects into our code

--- Module7/my-app2/src/App.js
+++ Module7/my-app2/src/App.js
@@ -1,30 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import logo from './logo.svg';
 import './App.css';
 
-function fetchUser() {
-  const url = `https://reqres.in/api/users/3`;
+function fetchUser(id) {
+  const url = `https://reqres.in/api/users/${id}`;
   return fetch(url)
     .then((res) => res.json())
     .then((res) => res.data);
 }
 
 function App() {
+  const [userId, setUserId] = useState(1);
   const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetchUser(userId).then((user) => setUser(user));
+  }, [userId]);
 
   return (
     <div className="App">
       <h1>User Details</h1>
-      <button onClick={() => fetchUser().then((user) => setUser(user))}>
-        Get User
+      <button disabled={userId === 1} onClick={() => setUserId(userId - 1)}>
+        Previous
       </button>
-      <ul>
-        <li>First Name: {user.first_name}</li>
-        <li>Last Name: {user.last_name}</li>
-        <li>E: {user.email}</li>
-      </ul>  
-    </div>
+      <button disabled={!userId === 1} onClick={() => setUserId(userId + 1)}>
+        Next
+      </button>
+      {user ? (
+        <div>
+          <img src={user.avatar} alt="Avatar"></img>
+          <ul>
+            <li>User id: {userId}</li>
+            <li>First Name: {user.first_name}</li>
+            <li>Last Name: {user.last_name}</li>
+            <li>E: {user.email}</li>
+          </ul>  
+        </div>
+  ) : (
+    <p>Loading...</p>
+  )}
+  </div>
   );
 }
 
