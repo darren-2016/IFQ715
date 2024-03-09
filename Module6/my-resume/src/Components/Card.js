671121cde13d5f0cf3c6a0a54ba74913785c4cd9
Darren Draper <darren.j.draper@gmail.com>
2024-03-09 14:36:49 +1000

    Added styling to Card

--- Module6/my-resume/src/Components/Card.js
+++ Module6/my-resume/src/Components/Card.js
@@ -1,17 +1,19 @@
 import { Container, Button, Card } from "react-bootstrap";
+// import { FontAwesomeIcon } from 
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "./styles.css";
 
 export default function BusinessCard ( {name, title, description} ) {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card border="primary" style={{ width: '18rem' }}>
           {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-          <Card.Header>{name}</Card.Header>
+          <Card.Header className="heading">{name}</Card.Header>
           <Card.Body>
             {/* <Card.Title>{ name }</Card.Title> */}
-            <Card.Subtitle className="mb-2 text-muted">{ title }</Card.Subtitle>
-            <Card.Text>{ description }</Card.Text>
+            <Card.Subtitle className="subtitle mb-2 text-muted">{ title }</Card.Subtitle>
+            <Card.Text className="description">{ description }</Card.Text>
             <Card.Link href="#">Email</Card.Link>
             <Card.Link href="#">LinkedIn</Card.Link>
 
