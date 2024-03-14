6bc7f364c58ede00c1c2016d6696c2612f0e58bb
Darren Draper <darren.j.draper@gmail.com>
2024-03-14 22:45:56 +1000

    Code-play 4: Login UI

--- Module5/my-spa/src/User/Login/index.js
+++ Module5/my-spa/src/User/Login/index.js
@@ -1,33 +1,72 @@
+
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 
 import TextField from "../TextField";
 
-export default function Login() {
- return (
-   <Container fluid="lg" className="pt-2">
-     <main className="flex-grow-1">
-       <Row className="viewport-height-75 align-items-center">
-         <Col md={6} lg={5} className="text-center">
-           <FontAwesomeIcon
-             icon={faDoorOpen}
-             className="page-icon text-secondary"
-           />
-         </Col>
-         <Col md={6} lg={7}>
-           <h1 className="mb-5">Login</h1>
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-           <Form>
-             <TextField text="Email" type="email" />
-             <TextField text="Password" type="password" />
-             <Button type="submit" variant="primary" className="mt-3">
-               Login
-             </Button>
-           </Form>
-         </Col>
-       </Row>
-     </main>
-   </Container>
- );
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    const API_URL = `https://d2h6rsg43otiqk.cloudfront.net/prod`;
+    const url = `${API_URL}/user/login`;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": "889c4757807b42ec98a21636231510",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          setMessage(data.message);
+        } else {
+          localStorage.setItem("token", data.token);
+          navigate({ pathname: "/" });
+        }
+      });
+    };
+
+  return (
+    <Container fluid="lg" className="pt-2">
+      <main className="flex-grow-1">
+        <Row className="viewport-height-75 align-items-center">
+          <Col md={6} lg={5} className="text-center">
+            <FontAwesomeIcon
+              icon={faDoorOpen}
+              className="page-icon text-secondary"
+            />
+          </Col>
+          <Col md={6} lg={7}>
+            <h1 className="mb-5">Login</h1>
+
+            <Form>
+              <TextField text="Email" type="email" onChange={setEmail} value={email}/>
+              <TextField text="Password" type="password" onChange={setPassword} value={password} />
+              <Button type="submit" variant="primary" className="mt-3">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </main>
+    </Container>
+  );
 }
